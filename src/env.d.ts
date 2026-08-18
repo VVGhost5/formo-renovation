f46df2636cf0b501ce9a1a307592f18cfc4b816c
mac <vvghost@ukr.net>
2026-08-18 23:21:09 +0300

    fixes studio v2

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,15 @@
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
 
+interface ImportMetaEnv {
+	readonly MAINTENANCE_MODE: string
+	readonly MAINTENANCE_SECRET: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
+
 declare module "*.vue" {
 	import type {DefineComponent} from "vue";
 	const component: DefineComponent<object, object, unknown>;
